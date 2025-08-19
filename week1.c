e67201a315faa5d6d467346c4c966af3c65f803c
mdsahilo <mohammedsahil78665@gmail.com>
2025-08-19 19:45:10 +0530

    Add files via upload

---
+++ week1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Taking input from user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Performing addition
+    sum = num1 + num2;
+
+    // Displaying result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+
